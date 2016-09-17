06eba9a262c65a3dfce74ba7d7edb2d2ae5c0ef5
heoegema <hollyoegema@gmail.com>
2016-09-17 12:47:19 -0400

    Added access to LUIS application

--- Sentiment.py
+++ Sentiment.py
@@ -4,10 +4,12 @@ import urllib
 import sys
 import base64
 import json
+import luis
 
 #Use LUIS to recognize intents 
 # Azure portal URL.
 base_url = 'https://westus.api.cognitive.microsoft.com/'
+luis_url = luis.Luis('https://api.projectoxford.ai/luis/v1/application?id=de15ad98-4809-4971-a30c-bb8470b6c78a&subscription-key=b0e5746863c04db4bb91e9a5583d2482')
 # Your account key goes here.
 account_key = '37cf49dc60bb435db8706acf05894bba'
 
@@ -15,21 +17,72 @@ headers = {'Content-Type':'application/json', 'Ocp-Apim-Subscription-Key':accoun
 
 
 def determineCourseOfAction(input):
-    if(classifyIntent(input)=="greeting"): #Might have to use LUIS for this
-        return "Greeting" #Jump to function to do things->Set a flag which we will keep globally
-def classifyIntent(input):
-    #UseKeyphrases for this maybe
-    #Can we use luis
-    #Greetings
-    #HowAreYou
-    #Making an Appointment
-    #Happy->Information gathering mood
-    #Sad
-    #Crisis
-    #Information of Mental Health Illnesses-Leave this to last
-    #Mental health institutions in the area
-    #Goodbye
-    print "classify Intent"
+    if(classifyIntent(input)=="Greeting"): #Might have to use LUIS for this
+        Greeting(input)#Jump to function to do things->Set a flag which we will keep globally
+    elif(classifyIntent(input)=="Information"):
+        Information(input) #Replace these with function calls
+    elif(classifyIntent(input)=="Bye"):
+        Bye(input)
+    elif(classifyIntent(input)=="Emotion"):
+        Emotion(input)
+    elif(classifyIntent(input)=="Schedule"):
+        Schedule(input)
+    elif(classifyIntent(input)=="HowAreYou"):
+        HowAreYou(input)
+    elif(classifyIntent(input)=="Store"):
+        Store(input)
+    else:
+        none(input)
+        
+def classifyIntent(input): 
+    
+    luisData = luis_url.analyze(input)
+    return luisData.best_intent().intent
+    
+#Functions that deal with intents 
+def Greeting(input):
+    
+    return "Hello"
+    
+def HowAreYou(input):
+    
+    return "Good"
+    
+def Schedule(input):
+    #need entities
+    return "Yes"
+    
+def Bye(input):
+    
+    return "Bye"
+    
+def Emotion(input):
+    #need entities 
+    #Based on emotion->might also use sentiment for this
+    #Happy->ask for information 
+    #Sad->defaults->
+    
+    return "ok"
+    
+def Information(input):
+    #Need entities
+    #Need stuff for certain mental illnesses, + coping strategies 
+    
+    
+    return "Ok"
+    
+def Store(input):
+    
+    #Note: Should we also store moods?
+    #this will probably store information in a database->can also check to do thing
+    return "OK"
+
+def none(input):
+    
+    return "OK"
+def Retrieve(type):
+    #This will retrieveFromdatabase
+    return "OK"
 def getMoodCheck(input):
     #This function will basically track the person's mood->we will do mood checks every once in a
     #while
@@ -61,8 +114,10 @@ def getKeyPhrases(input):
         #return key phrases
 
 def main():
-    getSentimentValue("Tonzo is my little baby and makes me happy")
-    getKeyPhrases("Hack the North")
+    print determineCourseOfAction("I am feeling sad")
+    getKeyPhrases("I am feeling sad")
+    #getSentimentValue("Tonzo is my little baby and makes me happy")
+    #getKeyPhrases("Hack the North")
     
     
 main()
