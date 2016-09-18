111f386aaf701f652da53d9b03bdcaebf5860f78
Mara Gagiu <maragagiu@v1020-wn-103-26.campus-dynamic.uwaterloo.ca>
2016-09-18 00:52:26 -0400

    created project and stared login screen

---
+++ Tonzo/Tonzo/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tonzo
+//
+//  Created by Mara Gagiu on 2016-09-18.
+//  Copyright © 2016 Mara Gagiu. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
